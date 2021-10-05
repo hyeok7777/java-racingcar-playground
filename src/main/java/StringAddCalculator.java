c5340ef74cd4731e8b23ea539d9e788351d5ed89
오진혁 <hyeok7777@gmail.com>
2021-10-05 22:19:49 +0900

    feat(StringAddCalculator) : StringAddCalculator production code

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,31 +1,82 @@
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
-
 import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static final int ZERO = 0;
-    private static final int ONE = 1;
-    String[] strNumbers;
-    private final String no;
-    CheckNumber checkNumber;
 
-    public StringAddCalculator(String no) {
-        this.no = no;
-        checkNumber = new CheckNumber(this.no);
+    public static int splitAndSum(String no) {
+
+        if (isNullOrEmpty(no)) {
+            return 0;
+        }
+
+        if (isLengthOne(no)) {
+            return Integer.parseInt(no);
+        }
+
+        if (isContainCommaOrColon(no)) {
+            return addCommaOrColon(no);
+        }
+
+        if (isContainCustom(no)) {
+            return addCustom(no);
+        }
+
+        return 0;
     }
 
-
-
-    private Integer makeSum(String[] strNumbers) {
-        int[] intNumbers = Arrays.asList(strNumbers).stream().mapToInt(Integer::parseInt).toArray();
-        return Arrays.stream(intNumbers).sum();
+    private static boolean isNullOrEmpty(String no) {
+        return no == null || no.isEmpty();
     }
 
-    public Integer splitAndSum() {
-
-
-        return makeSum(strNumbers);
+    private static boolean isLengthOne(String no) {
+        return no.length() == 1;
     }
+
+    private static boolean isContainCommaOrColon(String no) {
+        return no.contains(",") || no.contains(":");
+    }
+
+    private static int addCommaOrColon(String no) {
+        String[] numbers = no.split(",|:");
+        int[] nums = makeInteger(numbers);
+        return makeArraySum(nums);
+    }
+
+    private static int makeArraySum(int[] nums) {
+        return Arrays.stream(nums).sum();
+    }
+
+    private static boolean isContainCustom(String no) {
+        return no.contains("//");
+    }
+
+    private static int addCustom(String no) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(no);
+        m.find();
+        String customDelimiter = m.group(1);
+        String[] numbers = m.group(2).split(customDelimiter);
+        // 덧셈 구현
+        int[] nums = makeInteger(numbers);
+        return makeArraySum(nums);
+    }
+
+    private static int[] makeInteger(String[] numbers) {
+        int[] nums = Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
+
+        if(isNegative(nums)) {
+            throw new RuntimeException();
+        }
+
+        return nums;
+    }
+
+    private static boolean isNegative(int[] nums) {
+        if(Arrays.stream(nums).min().getAsInt() < 0) {
+            return true;
+        }
+        return false;
+    }
+
 }
